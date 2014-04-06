d31e3ffe18109296340e6c0f0ebe7389ff500455
Aline Caton <alinenepomuceno22@gmail.com>
2014-04-06 15:10:36 +1000

    Removing rails default class

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Chat::Application.initialize!
+
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
